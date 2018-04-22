62ad1a862be62d12759d244880641641aa9bef10
Matt Gramigna <mgramigna@umass.edu>
2018-04-22 12:10:22 -0400

    added some documentation

--- models.py
+++ models.py
@@ -3,6 +3,16 @@ from uuid import uuid4
 from collections import defaultdict
 
 class Precinct(object):
+    """
+    Class representing an individual precinct within a districts
+
+    Note:
+        self.id is automatically generated when a new precinct is created
+
+    Args:
+        party (str): identifies the party that this precinct voted with
+        district_id (str): the id of the district that this precinct is a party of
+    """
     def __init__(self, party, district_id):
         self.id = str(uuid4())
         self.party = party
@@ -17,6 +27,7 @@ class Districting(object):
 
     Note:
         self.n is assigned automatically based on the shape of the `grid` arg
+        self.shape is assigned automatically for ease of use in further iterating of the grid
 
     Args:
         grid (numpy.ndarray): a 2d array of tuples for each precinct in a districting
@@ -39,8 +50,7 @@ class Districting(object):
         Get all (non-diagonal) neighbors for a given precinct
 
         Args:
-            row (int): index for the row of the precinct
-            col (int): index for the column of the precinct
+            precinct: the precinct whose neighbors we wish to find
         """
         neighbors = []
         row,col = self.__index_of(precinct)
@@ -69,46 +79,101 @@ class Districting(object):
         return districts
 
     def is_valid(self):
+        """
+        Tests if a given districting is valid (simply connected and has contiguous districts)
+
+        Returns:
+            bool: True if the districting is valid, False otherwise
+        """
         return self.__is_contiguous() and self.__is_connected()
 
     def flip(self,precinct,neighbor,row,col):
+        """
+        Absorb a given precinct into the neighboring district
+
+        Args:
+            precinct (Precinct): the precinct being absorbed
+            neighbor (Precinct): the neighboring precinct in the district that is being absorbed
+            row (int): row of the precinct
+            col (int): column of the precinct
+        """
         self.grid[row,col] = Precinct(precinct.party, neighbor.district_id)
 
     def __is_contiguous(self):
+        """
+        Private function for testing if a grid contains contiguous districts
+
+        Returns:
+            bool: True if the number of contiguous precincts in the grid is equal to the number of total precincts, False otherwise
+        """
         contiguous_precinct_count = 0
         for row,col in np.ndindex(self.shape):
             neighbors = self.get_neighbors(self.grid[row,col])
             for neighbor in neighbors:
+
+                # if there is at least one neighbor with the same precinct, this precinct is contiguous
                 if neighbor.district_id == self.grid[row,col].district_id:
                     contiguous_precinct_count += 1
                     break
+
+        # all precincts should be contiguous
         return contiguous_precinct_count == self.n**2
 
     def __is_connected(self):
+        """
+        Private function for testing if the districts are simply connected
+
+        Returns:
+            bool: True if there are no breaks in the districts, False otherwise
+        """
+
+        # total number of precincts that fall under a given district
         precincts_per_district = defaultdict(lambda: defaultdict(int))
 
+        # calculcate the expected number of precincts we should see for a given district
         for row,col in np.ndindex(self.shape):
             precincts_per_district[self.grid[row,col].district_id]['expected'] += 1
 
         searched_districts = set()
         for row,col in np.ndindex(self.shape):
+
+            # don't search a district if we've already searched it before
             if self.grid[row,col].district_id in searched_districts:
                 continue
+
+            # start a depth first search from this point in the grid
             searcher = [self.grid[row,col]]
             current_district = self.grid[row,col].district_id
+
+            # don't search this district in further iterations
             searched_districts.add(current_district)
+
+            # find all neighbors with matching districts and mark them as seen
             seen = set()
             while len(searcher) > 0:
                 precinct = searcher.pop()
                 precincts_per_district[precinct.district_id]['seen'] += 1
                 seen.add(precinct.id)
-                searcher.extend([n for n in self.get_neighbors(precinct) if n.id not in seen and n.district_id == precinct.district_id])
+                searcher.extend([
+                    n for n in self.get_neighbors(precinct)
+                    if n.id not in seen and n.district_id == precinct.district_id
+                ])
+
+            # after we process this district, we should have searched all precincts with that distrcits if the grid is connected
             if precincts_per_district[current_district]['seen'] != precincts_per_district[current_district]['expected']:
                 return False
         return True
 
-
     def __index_of(self, precinct):
+        """
+        Find the matching row and column for a given precinct
+
+        Args:
+            precinct (Precinct): the precinct to search for
+
+        Returns:
+            tuple: coordinates of the precinct of the form (row,col). (-1,-1) if not found
+        """
         for row,col in np.ndindex(self.shape):
             if self.grid[row,col].id == precinct.id:
                 return (row,col)
--- simulation.py
+++ simulation.py
@@ -30,16 +30,28 @@ def simulate(length, dist):
     Args:
         length (int): number of steps to take in the walk
         dist (numpy.ndarray): original districting before the simulation
+
+    Returns:
     """
+
+    # get party voting counts before the simulatinos
     old_party_count = dist.get_party_count()
     for i in range(length):
+
+        # pick a random cell in the grid
         row, col = pick_rand_idx(dist.n), pick_rand_idx(dist.n)
         precinct = dist.grid[row,col]
         neighbors = dist.get_neighbors(precinct)
         for neighbor in neighbors:
+
+            # pick a neighbor with a different district id
             if neighbor.district_id != precinct.district_id:
+
+                # create a proposed districting as if we were to flip this precinct with the neighbor
                 proposed_dist = Districting(copy(dist.grid))
                 proposed_dist.grid[row,col] = Precinct(precinct.party, neighbor.district_id)
+
+                # apply the flip to the original districting if the proposed districting is valid
                 if proposed_dist.is_valid():
                     dist.flip(precinct, neighbor, row, col)
                     break
